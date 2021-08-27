b6b0e86dd7dccc8c492ef9c0a29d414c486d27ed
Ian Phua <ianphua3@gmail.com>
2021-08-27 15:09:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const cors = require('cors')
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('HELLO SERVER')
+})
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', socket => {
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('Call ended')
+    })
+    
+    socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('callUser', { signal: signalData, from, name })
+    })
+
+    socket.on('answercall', data => {
+        io.to(data.to).emit('callaccepted', data.signal)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('Listening on', PORT)
+})

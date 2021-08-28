92d86af4bf87fa3021771bd7b9eb5e70b616c0e0
Ian Phua <ianphua3@gmail.com>
2021-08-28 13:39:55 -0400

    Implemented notifications logic

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import { useVideoCallContext } from '../SocketContext'
 
 const Notifications = () => {
+    const { call, answerCall, callAccepted } = useVideoCallContext()
+
     return (
-        <div>
-            Notifications
-        </div>
+        <>
+            {call.isReceivingCall && !callAccepted && (
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <h1>{call.name} is calling: </h1>
+                    <Button variant='contained' color='primary' onClick={answerCall}>
+                        Answer
+                    </Button>
+                </div>
+            )}
+        </>
     )
 }
 

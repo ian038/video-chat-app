1803a4faf63ad526f013a64c2345b3e573866f7c
Ian Phua <ianphua3@gmail.com>
2021-08-28 13:47:40 -0400

    Deploying server

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -31,7 +31,7 @@ const VideoPlayer = () => {
             {stream && (
                 <Paper className={classes.paper}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
+                        <Typography variant="h5" gutterBottom>{name}</Typography>
                         <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
                     </Grid>
                 </Paper>
@@ -39,7 +39,7 @@ const VideoPlayer = () => {
             {callAccepted && !callEnded && (
                 <Paper className={classes.paper}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant='h5' gutterBottom>{call.name || 'Name'}</Typography>
+                        <Typography variant='h5' gutterBottom>{call.name}</Typography>
                         <video playsInline ref={userVideo} autoPlay className={classes.video} />
                     </Grid>
                 </Paper>

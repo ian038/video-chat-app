bf016a33ffee2156e69d881b152ae2f4cc10e112
Ian Phua <ianphua3@gmail.com>
2021-08-28 14:02:32 -0400

    Deployed app

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext()
 
-const socket = io('http://localhost:5000')
+const socket = io('https://realtime-video-chat-app-server.herokuapp.com/')
 
 function useProvideContext() {
     const [stream, setStream] = useState(null)
